3c9e3f94207f94f70676c3ac0e8d5a101b047582
Mahammad Rahmanov <maqomed.rahmanov@gmail.com>
2021-09-21 19:59:53 +0200

    Small design change

--- src/components/Error.js
+++ src/components/Error.js
@@ -4,7 +4,7 @@ function Error() {
     const isIncorrect = useContext(WeatherData)
     return (
 
-        isIncorrect[0] ? (<p className="flex justify-center bg-red-400 rounded p-2 mt-2 w-1/3">Enter correct city name</p>)
+        isIncorrect[0] ? (<p className="flex justify-center bg-red-400 rounded p-2 mt-2 md:w-1/3">Enter correct city name</p>)
             : (<div></div>)
 
     )
